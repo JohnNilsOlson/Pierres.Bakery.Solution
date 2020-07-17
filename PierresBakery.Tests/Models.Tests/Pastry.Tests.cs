353474c10b53a99033d97309be5769317449a6a2
JohnNils Olson <johnnils@gmail.com>
2020-07-17 10:05:23 -0700

    add price variable to pastry constructor test

--- PierresBakery.Tests/Models.Tests/Pastry.Tests.cs
+++ PierresBakery.Tests/Models.Tests/Pastry.Tests.cs
@@ -9,15 +9,22 @@ namespace Pastries.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry("croissant");
+      Pastry newPastry = new Pastry("croissant", 2);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastryWithType_String()
     {
-      Pastry newPastry = new Pastry("croissant");
+      Pastry newPastry = new Pastry("croissant", 2);
       Assert.AreEqual("croissant", newPastry.Type);
+    }
+
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastryWithPrice_Int()
+    {
+      Pastry newPastry = new Pastry("croissant", 2);
+      Assert.AreEqual(2, newPastry.Price);
     }
   }
 }
