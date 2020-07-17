d222441838e030e1d1726acce7d2352beef9eda6
JohnNils Olson <johnnils@gmail.com>
2020-07-17 11:42:51 -0700

    add bread and pastry lists to menu constructor

--- PierresBakery/Models/Menu.cs
+++ PierresBakery/Models/Menu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using PierresBakery.Models.Breads;
 using PierresBakery.Models.Pastries;
 
@@ -5,9 +6,12 @@ namespace PierresBakery.Models.Menus
 {
   public class Menu
   {
+    public List<Bread> Breads { get; set; }
+    public List<Pastry> Pastries {get; set; }
     public Menu()
     {
-
+      Breads = new List<Bread>();
+      Pastries = new List<Pastry>();
     }
   }
 }
