c10e398fc96b946266b432440e53d79d20d6054f
JohnNils Olson <johnnils@gmail.com>
2020-07-17 10:07:12 -0700

    add code to pastry constructor to pass price test

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,9 +3,11 @@ namespace PierresBakery.Models.Pastries
   public class Pastry
   {
     public string Type { get; set; }
-    public Pastry(string type)
+    public int Price { get; set; }
+    public Pastry(string type, int price)
     {
       Type = type;
+      Price = price;
     }
   }
 }
